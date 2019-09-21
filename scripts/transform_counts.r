777f7a2ed54bb3e1c6bafe14ef92e195bb3f1616
Jen Wisecaver <jwisecav@purdue.edu>
2019-09-21 09:49:48 -0400

    update transform_counts Rscript

---
+++ scripts/transform_counts.r
@@ -0,0 +1,139 @@
+## 20190921
+## Jen Wisecaver
+## Normalize and transform an RNAseq counts matrix
+##
+## See transform_count jupyter notebook for walkthrough
+## Usage: Rscript transform_counts.R counts.matrix conditions.txt
+
+library('getopt')
+# get options, using the spec as defined by the enclosed list.
+# we read the options from the default: commandArgs(TRUE).
+spec = matrix(c(
+  'help'        , 'h', 0, "logical", '',
+  'abundances'  , 'a', 1, "character", 'Enter list of abundance estimates from Kallisto (see jupyter notebook tutorial)',
+  'lookup'      , 'l', 1, "character", 'Enter transcript to gene lookup file (see jupyter notebook tutorial)',
+  'samples'     , 's', 1, "character", 'Enter experimental design file (see jupyter notebook tutorial)',
+  'seqtype'     , 't', 1, "character", 'Full-length or 3 prime tagged RNAseq? Enter [Full/full/F/f or Tag/tag/T/t]'
+), byrow=TRUE, ncol=5)
+opt = getopt(spec)
+
+# if help was asked for print a friendly message
+# and exit with a non-zero error code
+if ( is.null(opt$abundances) || is.null(opt$lookup) || is.null(opt$samples) || is.null(opt$seqtype)) {
+  cat(getopt(spec, usage=TRUE))
+  q(status=1)
+}
+
+if ( !is.null(opt$help) ) {
+  cat(getopt(spec, usage=TRUE))
+  q(status=1)
+}
+
+filesfile <- opt$abundances
+#cat(system((paste('head -n 5', filesfile)), intern=TRUE), sep='\n')
+
+library(stringr)
+files <- scan(filesfile, what = 'list')
+file_names <- str_split(files, '/', simplify = TRUE)
+file_names <- file_names[,ncol(file_names)-1]
+names(files) <- file_names
+#head(files)
+
+t2gfile <- opt$lookup
+#cat(system((paste('head -n 5', t2gfile)), intern=TRUE), sep='\n')
+
+tx2gene <- read.table(t2gfile, sep="\t", header=TRUE)
+#head(tx2gene)
+
+samplefile <- opt$samples
+#cat(system((paste('head -n 5', samplefile)), intern=TRUE), sep='\n')
+
+sampleTable <- read.table(samplefile, sep="\t", header=TRUE)
+#head(sampleTable)
+
+CFA <- 'no'
+seqtype <- opt$seqtype
+
+if (seqtype == 'full' || seqtype == 'Full' || seqtype == 'F' || seqtype == 'f'){
+	CFA <- 'scaledTPM'
+} 
+
+if (seqtype == 'tag' || seqtype == 'Tag' || seqtype == 'T' || seqtype == 't'){
+	CFA <- 'no'
+} 
+
+library(tximport)
+library(rhdf5)
+paste('tximport options: type = kallisto, tx2gene = tx2gene, countsFromAbundance = ', CFA)
+txi <- tximport(files, type = "kallisto", tx2gene = tx2gene, countsFromAbundance = CFA)
+#names(txi)
+
+
+library("DESeq2")
+dds <- DESeqDataSetFromTximport(txi, sampleTable, ~condition)
+dds <- estimateSizeFactors(dds)
+#head(counts(dds))
+
+#nrow(dds)
+dds_filt <- dds[ rowSums(counts(dds)) > 0, ]
+#nrow(dds_filt)
+
+normalized_matrix <- counts(dds_filt, normalized = TRUE)
+#head(normalized_matrix)
+
+vsd <- vst(dds_filt, blind = FALSE)
+normalized_vst_matrix <- assay(vsd)
+#head(normalized_vst_matrix)
+
+# Tally number of genes with expression greater than zero
+df_binary <- normalized_matrix
+df_binary[df_binary == 0] <- 0
+df_binary[df_binary > 0] <- 1
+stats_df <- as.data.frame(rowSums(df_binary))
+names(stats_df) <- c('no. samples with count > 0')
+
+# Tally number of genes with expression greater than or equal to five
+df_binary <- normalized_matrix
+df_binary[df_binary < 5] <- 0
+df_binary[df_binary > 0] <- 1
+stats_df$'no. samples with count >= 5' = rowSums(df_binary)
+
+# Tally number of genes with expression greater than or equal to ten
+df_binary <- normalized_matrix
+df_binary[df_binary < 10] <- 0
+df_binary[df_binary > 0] <- 1
+stats_df$'no. samples with count >= 10' = rowSums(df_binary)
+
+# Calculate median expression genes in the normalized matrix
+median_expression  <- apply(normalized_matrix, 1, median)
+stats_df$'pre-VST median expression' = median_expression
+
+# Calculate mean expression genes in the normalized matrix
+mean_expression  <- apply(normalized_matrix, 1, mean)
+stats_df$'pre-VST mean expression' = mean_expression
+
+# Calculate expression variance genes in the normalized matrix
+exp_variance <- apply(normalized_matrix, 1, var)
+stats_df$'pre-VST expression variance' = exp_variance
+
+# Calculate median expression genes in the VST matrix
+median_expression  <- apply(normalized_vst_matrix, 1, median)
+stats_df$'post-VST median expression' = median_expression
+
+# Calculate mean expression genes in the VST matrix
+mean_expression  <- apply(normalized_vst_matrix, 1, mean)
+stats_df$'post-VST mean expression' = mean_expression
+
+# Calculate expression variance genes in the VST matrix
+exp_variance <- apply(normalized_vst_matrix, 1, var)
+stats_df$'post-VST expression variance' = exp_variance
+#head(stats_df)
+
+norm_outfil = 'gene_counts_normalized.matrix'
+write.table(normalized_matrix, norm_outfil, sep="\t", quote = FALSE)
+
+normVst_outfil = 'gene_counts_normalized_vst_transformed.matrix'
+write.table(normalized_vst_matrix, normVst_outfil, sep="\t", quote = FALSE)
+
+stats_outfil = 'gene_counts_statistics.txt'
+write.table(stats_df, stats_outfil, sep="\t", quote = FALSE)
